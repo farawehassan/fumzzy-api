49119fca156fa90ce652f6ef7e0821cb08f67b9f
farawehassan <farawehassan@yahoo.com>
2021-11-08 15:33:23 +0100

    Fixed store charts error

--- controllers/storeDetails.js
+++ controllers/storeDetails.js
@@ -1,9 +1,9 @@
-const Product = require('../models/product')
-const Sales = require('../models/sales')
-const Customer = require('../models/customer')
-const Purchases = require('../models/purchases')
-const Expenses = require('../models/expenses')
-const Creditors = require('../models/creditor')
+const Product = require("../models/product")
+const Sales = require("../models/sales")
+const Customer = require("../models/customer")
+const Purchases = require("../models/purchases")
+const Expenses = require("../models/expenses")
+const Creditors = require("../models/creditor")
 const Helpers = require("../helpers/helper")
 
 // Fetch store details
@@ -19,8 +19,7 @@ exports.fetchDetails = async (req, res, next) => {
 
   try {
     const products = await Product.find()
-    const sales = await Sales.find()
-    const customers = await Customer.find({ 'reports.paid': false })
+    const customers = await Customer.find({ "reports.paid": false })
     const creditors = await Creditors.find()
 
 
@@ -31,13 +30,6 @@ exports.fetchDetails = async (req, res, next) => {
       totalItems += parseFloat(products[i].currentQty)
     }
 
-    /// Sales section
-    for (i = 0; i < sales.length; i++) {
-      totalSalesMade += parseFloat(sales[i].totalPrice);
-      totalProfitMade += parseFloat(sales[i].quantity) * (parseFloat(sales[i].unitPrice) - parseFloat(sales[i].costPrice))
-    }
-
-  
     /// Customer section
     for (i = 0; i < customers.length; i++) { 
       for(j = 0; j < customers[i].reports.length; j++) {
@@ -66,10 +58,10 @@ exports.fetchDetails = async (req, res, next) => {
       outstandingPurchaseVolume: outstandingPurchaseVolume
     }; 
 
-    return res.status(200).send({ error: false, message: 'Store details successfully fetched', data: storeDetails }); 
+    return res.status(200).send({ error: false, message: "Store details successfully fetched", data: storeDetails }); 
   } catch (err) {
     console.log(err);
-    return res.status(500).send({ error: true, message: 'Database operation failed, please try again' });
+    return res.status(500).send({ error: true, message: "Database operation failed, please try again" });
   }
 
 } 
@@ -83,178 +75,243 @@ exports.fetchDetailsChart = async (req, res, next) => {
 
 
     /// Sales section
-    const todaySales = await Sales.aggregate[
+    const todaySales = await Sales.aggregate([
       {
         $match: { createdAt: { $gte: Helpers.getCurrentTimestamp(0), $lte: Helpers.getCurrentEndDate() } }
       },
       {
-        $group: { _id: null, total: { $sum: '$totalPrice' } },
+        $group: { _id: null, total: { $sum: "$totalPrice" } },
       }
-    ]
+    ])
 
-    const yesterdaySales = await Sales.aggregate[
+    const yesterdaySales = await Sales.aggregate([
       {
         $match: { createdAt: { $gte: Helpers.getCurrentTimestamp(-1), $lte: Helpers.getCurrentDate() } }
       },
       {
-        $group: { _id: null, total: { $sum: '$totalPrice' } },
+        $group: { _id: null, total: { $sum: "$totalPrice" } },
       }
-    ]
+    ])
 
-    const weekSales = await Sales.aggregate[
+    const weekSales = await Sales.aggregate([
       {
         $match: { createdAt: { $gte: Helpers.getCurrentTimestamp(-7), $lte: new Date() } }
       },
       {
-        $group: { _id: null, total: { $sum: '$totalPrice' } },
+        $group: { _id: null, total: { $sum: "$totalPrice" } },
       }
-    ]
+    ])
 
-    const monthSales = await Sales.aggregate[
+    const monthSales = await Sales.aggregate([
       {
         $match: { createdAt: { $gte: Helpers.getCurrentTimestamp(-31), $lte: new Date() } }
       },
       {
-        $group: { _id: null, total: { $sum: '$totalPrice' } },
+        $group: { _id: null, total: { $sum: "$totalPrice" } },
       }
-    ]
+    ])
 
-    const sixMonthSales = await Sales.aggregate[
+    const sixMonthSales = await Sales.aggregate([
       {
         $match: { createdAt: { $gte: Helpers.getCurrentTimestamp(-184), $lte: new Date() } }
       },
       {
-        $group: { _id: null, total: { $sum: '$totalPrice' } },
+        $group: { _id: null, total: { $sum: "$totalPrice" } },
       }
-    ]
+    ])
 
-    const allSales = await Sales.aggregate[
+    const allSales = await Sales.aggregate([
       {
-        $group: { _id: null, total: { $sum: '$totalPrice' } },
+        $group: { _id: null, total: { $sum: "$totalPrice" } },
       }
-    ]
+    ])
+
+
+
+    /// Profit section
+    const todayProfit = await Sales.aggregate([
+      {
+        $match: { createdAt: { $gte: Helpers.getCurrentTimestamp(0), $lte: Helpers.getCurrentEndDate() } }
+      },
+      {
+        $group: { _id: null, total: { $sum: { 
+          $multiply : [ { $subtract: [ "$unitPrice", "$costPrice" ] }, "$quantity" ] 
+        } } },
+      }
+    ])
+
+    const yesterdayProfit = await Sales.aggregate([
+      {
+        $match: { createdAt: { $gte: Helpers.getCurrentTimestamp(-1), $lte: Helpers.getCurrentDate() } }
+      },
+      {
+        $group: { _id: null, total: { $sum: { 
+          $multiply : [ { $subtract: [ "$unitPrice", "$costPrice" ] }, "$quantity" ] 
+        } } },
+      }
+    ])
+
+    const weekProfit = await Sales.aggregate([
+      {
+        $match: { createdAt: { $gte: Helpers.getCurrentTimestamp(-7), $lte: new Date() } }
+      },
+      {
+        $group: { _id: null, total: { $sum: { 
+          $multiply : [ { $subtract: [ "$unitPrice", "$costPrice" ] }, "$quantity" ] 
+        } } },
+      }
+    ])
+
+    const monthProfit = await Sales.aggregate([
+      {
+        $match: { createdAt: { $gte: Helpers.getCurrentTimestamp(-31), $lte: new Date() } }
+      },
+      {
+        $group: { _id: null, total: { $sum: { 
+          $multiply : [ { $subtract: [ "$unitPrice", "$costPrice" ] }, "$quantity" ] 
+        } } },
+      }
+    ])
+
+    const sixMonthProfit = await Sales.aggregate([
+      {
+        $match: { createdAt: { $gte: Helpers.getCurrentTimestamp(-184), $lte: new Date() } }
+      },
+      {
+        $group: { _id: null, total: { $sum: { 
+          $multiply : [ { $subtract: [ "$unitPrice", "$costPrice" ] }, "$quantity" ] 
+        } } },
+      }
+    ])
+
+    const allProfit = await Sales.aggregate([
+      {
+        $group: { _id: null, total: { $sum: { 
+          $multiply : [ { $subtract: [ "$unitPrice", "$costPrice" ] }, "$quantity" ] 
+        } } },
+      }
+    ])
 
 
 
     /// Expenses section
-    const todayExpenses = await Expenses.aggregate[
+    const todayExpenses = await Expenses.aggregate([
       {
         $match: { createdAt: { $gte: Helpers.getCurrentTimestamp(0), $lte: Helpers.getCurrentEndDate() } }
       },
       {
-        $group: { _id: null, total: { $sum: '$amount' } },
+        $group: { _id: null, total: { $sum: "$amount" } },
       }
-    ]
+    ])
 
-    const yesterdayExpenses = await Expenses.aggregate[
+    const yesterdayExpenses = await Expenses.aggregate([
       {
         $match: { createdAt: { $gte: Helpers.getCurrentTimestamp(-1), $lte: Helpers.getCurrentDate() } }
       },
       {
-        $group: { _id: null, total: { $sum: '$amount' } },
+        $group: { _id: null, total: { $sum: "$amount" } },
       }
-    ]
+    ])
 
-    const weekExpenses = await Expenses.aggregate[
+    const weekExpenses = await Expenses.aggregate([
       {
         $match: { createdAt: { $gte: Helpers.getCurrentTimestamp(-7), $lte: new Date() } }
       },
       {
-        $group: { _id: null, total: { $sum: '$amount' } },
+        $group: { _id: null, total: { $sum: "$amount" } },
       }
-    ]
+    ])
 
-    const monthExpenses = await Expenses.aggregate[
+    const monthExpenses = await Expenses.aggregate([
       {
         $match: { createdAt: { $gte: Helpers.getCurrentTimestamp(-31), $lte: new Date() } }
       },
       {
-        $group: { _id: null, total: { $sum: '$amount' } },
+        $group: { _id: null, total: { $sum: "$amount" } },
       }
-    ]
+    ])
 
-    const sixMonthExpenses = await Expenses.aggregate[
+    const sixMonthExpenses = await Expenses.aggregate([
       {
         $match: { createdAt: { $gte: Helpers.getCurrentTimestamp(-184), $lte: new Date() } }
       },
       {
-        $group: { _id: null, total: { $sum: '$amount' } },
+        $group: { _id: null, total: { $sum: "$amount" } },
       }
-    ]
+    ])
 
-    const allExpenses = await Expenses.aggregate[
+    const allExpenses = await Expenses.aggregate([
       {
-        $group: { _id: null, total: { $sum: '$amount' } },
+        $group: { _id: null, total: { $sum: "$amount" } },
       }
-    ]
+    ])
 
 
     /// Purchases section
-    const todayPurchases = await Purchases.aggregate[
+    const todayPurchases = await Purchases.aggregate([
       {
         $match: { createdAt: { $gte: Helpers.getCurrentTimestamp(0), $lte: Helpers.getCurrentEndDate(0) } }
       },
       {
-        $group: { _id: null, total: { $sum: '$costPrice' } },
+        $group: { _id: null, total: { $sum: "$costPrice" } },
       }
-    ]
+    ])
 
-    const yesterdayPurchases = await Purchases.aggregate[
+    const yesterdayPurchases = await Purchases.aggregate([
       {
         $match: { createdAt: { $gte: Helpers.getCurrentTimestamp(-1), $lte: Helpers.getCurrentDate() } }
       },
       {
-        $group: { _id: null, total: { $sum: '$costPrice' } },
+        $group: { _id: null, total: { $sum: "$costPrice" } },
       }
-    ]
+    ])
 
-    const weekPurchases = await Purchases.aggregate[
+    const weekPurchases = await Purchases.aggregate([
       {
         $match: { createdAt: { $gte: Helpers.getCurrentTimestamp(-7), $lte: new Date() } }
       },
       {
-        $group: { _id: null, total: { $sum: '$costPrice' } },
+        $group: { _id: null, total: { $sum: "$costPrice" } },
       }
-    ]
+    ])
 
-    const monthPurchases = await Purchases.aggregate[
+    const monthPurchases = await Purchases.aggregate([
       {
         $match: { createdAt: { $gte: Helpers.getCurrentTimestamp(-31), $lte: new Date() } }
       },
       {
-        $group: { _id: null, total: { $sum: '$costPrice' } },
+        $group: { _id: null, total: { $sum: "$costPrice" } },
       }
-    ]
+    ])
 
-    const sixMonthPurchases = await Purchases.aggregate[
+    const sixMonthPurchases = await Purchases.aggregate([
       {
         $match: { createdAt: { $gte: Helpers.getCurrentTimestamp(-184), $lte: new Date() } }
       },
       {
-        $group: { _id: null, total: { $sum: '$costPrice' } },
+        $group: { _id: null, total: { $sum: "$costPrice" } },
       }
-    ]
+    ])
 
-    const allPurchases = await Purchases.aggregate[
+    const allPurchases = await Purchases.aggregate([
       {
-        $group: { _id: null, total: { $sum: '$costPrice' } },
+        $group: { _id: null, total: { $sum: "$costPrice" } },
       }
-    ]
+    ])
 
     var storeDetails = {
-      yesterday: [yesterdaySales, yesterdaySales, yesterdayExpenses, yesterdayPurchases],
-      today: [todaySales, todaySales, todayExpenses, todayPurchases],
-      thisMonth: [monthSales, monthSales, monthExpenses, monthPurchases],
-      sixMonth: [sixMonthSales, sixMonthSales, sixMonthExpenses, sixMonthPurchases],
-      allTime: [allSales, allSales, allExpenses, allPurchases],
+      yesterday: [yesterdaySales, yesterdayProfit, yesterdayExpenses, yesterdayPurchases],
+      today: [todaySales, todayProfit, todayExpenses, todayPurchases],
+      week: [weekSales, weekProfit, weekExpenses, weekPurchases],
+      thisMonth: [monthSales, monthProfit, monthExpenses, monthPurchases],
+      sixMonth: [sixMonthSales, sixMonthProfit, sixMonthExpenses, sixMonthPurchases],
+      allTime: [allSales, allProfit, allExpenses, allPurchases],
     }
-    
-    console.log(storeDetails)
 
-    return res.status(200).send({ error: false, message: 'Store details successfully fetched', data: storeDetails }); 
+    return res.status(200).send({ error: false, message: "Store details successfully fetched", data: storeDetails }); 
   } catch (err) {
     console.log(err);
-    return res.status(500).send({ error: true, message: 'Database operation failed, please try again' });
+    return res.status(500).send({ error: true, message: "Database operation failed, please try again" });
   }
 
 } 
